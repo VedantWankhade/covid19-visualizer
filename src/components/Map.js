319228437d798b9843815f91600315c9dcb650a0
VedantWankhade <vedantwankhade17@gmail.com>
2021-08-01 16:42:26 +0530

    .env

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,5 @@
-const Map = props => {
-    const data = props.covidData;
-    console.log(data)
-    return (
-        <div>
-        <h2>totalCases: {data.totalCases}</h2>
-        <h2>totalCases: {data.todayRecovered}</h2>
-        <h2>totalDeaths: {data.totalDeaths}</h2>
-        </div>
-    )
+const Main = () => {
+    return <h1>Main Page</h1>
 }
 
-export default Map;
+export default Main;
