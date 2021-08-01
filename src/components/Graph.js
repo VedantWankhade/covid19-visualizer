db9ceba772b63229c75d0f5fff947ab290b0f168
VedantWankhade <vedantwankhade17@gmail.com>
2021-08-01 13:50:31 +0530

    Testing CancvasJSReact charts

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,5 +1,24 @@
+import CanvasJSReact from "../lib/canvasjs.react";
+
 const Graph = () => {
-    return <h1>Graph page</h1>
+    let CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+    const options = {
+        title: {
+            text: 'Test chart'
+        },
+        data: [{
+            type: 'column',
+            dataPoints: [
+                { label: "Apple",  y: 10  },
+                { label: "Orange", y: 15  },
+                { label: "Banana", y: 25  },
+                { label: "Mango",  y: 30  },
+                { label: "Grape",  y: 28  }
+            ]
+        }]
+    };
+    return (<CanvasJSChart options={options} />)
 }
 
 export default Graph;
